770e6e591355d69850e12fda219c293c659ec8ec
Victor Hugo <victorsilveira.matos@gmail.com>
2025-06-06 12:08:52 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Nahiara, eu amo você, se você me ama, dá uma risadinha 😁💜</h1>
+  return <h1>Nahiara, eu amo você, se você me ama, dá uma risadinha 😁💜</h1>;
 }
 
 export default Home;
